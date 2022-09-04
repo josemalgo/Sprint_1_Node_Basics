dcf704844991eaffe86ec38fd6d3db96d1beed6e
jose1432@gmail.com <jose1432@gmail.com>
2022-09-04 19:44:42 +0200

    crypto

--- Entrega1.5_Node_Utils.js
+++ Entrega1.5_Node_Utils.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const AdmZip = require('adm-zip');
 const { exec } = require('child_process');
+const { scrypt, randomFill, createCipheriv } = await import('node:crypto');
 
 //------------------- NIVELL 1 - EXERCICI 1 ------------------------
 
@@ -55,16 +56,85 @@ const { exec } = require('child_process');
 
 //------------------- NIVELL 2 - EXERCICI 2 ------------------------
 
-exec('dir ', (error, stdout, stderr ) => {
-    if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-    }
+// exec('dir ', (error, stdout, stderr ) => {
+//     if (error) {
+//         console.error(`error: ${error.message}`);
+//         return;
+//     }
 
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
 
-    console.log(`Salida: ${stdout}`);
-});
+//     console.log(`Salida: ${stdout}`);
+// });
+
+//------------------- NIVELL 3 - EXERCICI 1 ------------------------
+
+async function encodingFiles() { 
+    
+    const data = await readFileEx1('helloWorld.txt'); 
+
+    if (data){
+        const buf = Buffer.from(data, 'utf8');
+
+        fs.writeFile('n3e1Hex.txt', buf.toString('hex'), function (err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+
+        fs.writeFile('n3e1B64.txt', buf.toString('base64'), function (err) {
+            if(err) {
+                return console.log(err);
+            }
+        });
+    }
+    
+}
+
+async function readFileEx1(file) {
+    
+    try {
+        return await fs.readFile(file, 'utf8');
+    }
+    catch (error) {
+        console.error(error.message);
+    }
+}
+
+encodingFiles();
+
+
+//------------------- NIVELL 3 - EXERCICI 2 ------------------------
+
+async function cipherFile() {
+
+    const algorithm = 'aes-192-cbc';
+    const password = 'Password used to generate key';
+
+    scrypt(password, 'salt', 24, (err, key) => {
+        if (err) throw err;
+
+        randomFill(new Uint8Array(16), (err, iv) => {
+            if (err) throw err;
+
+            const cipher = createCipheriv(algorithm, key, iv);
+
+            data = readFileEx1('n3e1B64.txt')
+            let encrypted = cipher.update(data, 'utf8', 'hex');
+            encrypted += cipher.final('hex');
+            
+            fs.writeFile('B64ToAes.txt', encrypted, function (err) {
+                if(err) {
+                    return console.log(err);
+                }
+            })
+
+        })
+    })
+
+}
+
+cipherFile();
